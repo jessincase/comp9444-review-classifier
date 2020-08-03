75f654925838aeaca4bb29af8fd35311eff56394
jessincase <au_jessica.huang@hotmail.com>
2020-08-03 14:59:28 +1000

    add CNN model.

--- student.py
+++ student.py
@@ -18,10 +18,12 @@ The variable device may be used to refer to the CPU/GPU being used by PyTorch.
 You may only use GloVe 6B word vectors as found in the torchtext package.
 """
 
-# import torch
+import torch
 import torch.nn as tnn
+import torch.nn.functional as F
 import torch.optim as toptim
 from torchtext.vocab import GloVe
+import tensorflow as tf
 # import numpy as np
 # import sklearn
 
@@ -33,6 +35,7 @@ def preprocessing(sample):
     """
     Called after tokenising but before numericalising.
     """
+    # negative sampling
 
     return sample
 
@@ -40,11 +43,11 @@ def postprocessing(batch, vocab):
     """
     Called after numericalisation but before vectorisation.
     """
-
+    
     return batch
 
 stopWords = {}
-wordVectors = GloVe(name='6B', dim=50)
+wordVectors = GloVe(name='6B', dim=300)
 
 ###########################################################################
 ##### The following determines the processing of label data (ratings) #####
@@ -59,7 +62,7 @@ def convertLabel(datasetLabel):
     Consider regression vs classification.
     """
 
-    return datasetLabel
+    return datasetLabel.sub_(1)
 
 def convertNetOutput(netOutput):
     """
@@ -69,8 +72,9 @@ def convertNetOutput(netOutput):
     If your network outputs a different representation or any float
     values other than the five mentioned, convert the output here.
     """
+    netOutput = netOutput.argmax(dim=1, keepdim=True)
 
-    return netOutput
+    return netOutput.sub_(-1)
 
 ###########################################################################
 ################### The following determines the model ####################
@@ -86,9 +90,35 @@ class network(tnn.Module):
 
     def __init__(self):
         super(network, self).__init__()
+        class_num = 5
+        filters_num = 100
+        kernel_sizes = [3,4,5]
+        dropout = 0.5 
+        embed_dim = 300
+        
+        self.convs = tnn.ModuleList([
+            tnn.Conv2d(1, filters_num, (size, embed_dim)) 
+            for size in kernel_sizes
+        ])
+        self.dropout = tnn.Dropout(dropout)
+        self.fc = tnn.Linear(len(kernel_sizes) * filters_num, class_num)
 
-    def forward(self, input, length):
-        pass
+
+    def forward(self, x, length):
+        # [(W−K+2P)/S]+1.
+        
+        # padding = tnn.ZeroPad2d((0, 0, 0, 125-x.size(1)))
+        x = x.unsqueeze(1)
+        # x = padding(x)
+
+        x = [F.relu(conv(x)).squeeze(3) for conv in self.convs] # [(batch_size, kernel_num, h)]
+        x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x] # [(batch_size, kernel_num)]
+        x = torch.cat(x, 1) # (batch_size, kernel_num * len(kernel_sizes)) 
+        x = self.dropout(x)
+        x = self.fc(x)
+        
+        return x
+        
 
 class loss(tnn.Module):
     """
@@ -100,7 +130,8 @@ class loss(tnn.Module):
         super(loss, self).__init__()
 
     def forward(self, output, target):
-        pass
+        return F.cross_entropy(output, target.type(torch.LongTensor))
+        #return F.nll_loss(output, target.type(torch.LongTensor))
 
 net = network()
 """
@@ -116,4 +147,7 @@ lossFunc = loss()
 trainValSplit = 0.8
 batchSize = 32
 epochs = 10
-optimiser = toptim.SGD(net.parameters(), lr=0.01)
+# optimiser = toptim.Adam(params=net.parameters(),
+#                                      lr=1e-4,
+#                                      weight_decay=1e-5)
+optimiser = toptim.SGD(net.parameters(), lr=0.01, momentum=0.01)
